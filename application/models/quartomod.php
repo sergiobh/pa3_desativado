67f9a355a8d54b82177226a31eddfbf11d0c98ae
sergio <sergiobh@gmail.com>
2013-04-24 17:03:04 -0300

    Criação das consultas de Quarto

--- application/models/quartomod.php
+++ application/models/quartomod.php
@@ -8,8 +8,29 @@ class QuartoMod extends CI_Model{
 
 
     public function Listar(){
-        
-        
+        $sql    = "
+                    SELECT
+                        Q.QuartoId
+                        ,Q.Andar
+                        ,Q.Identificacao AS Quarto        
+                        ,IF(Q.Status = 0, 'Desativado',  'Ativo') AS Status
+                    FROM
+                        quarto Q
+                    ORDER BY
+                        Q.Andar
+                        ,Q.Identificacao
+                    ";
+
+        $query  = $this->db->query($sql);
+
+        $dados = $query->result();
+
+        if(count($dados) > 0){
+            return $dados;
+        }
+        else{
+            return false;
+        }
     }
 }
 ?>
