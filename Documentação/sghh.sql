5070aadd11f0ce9f4796694c2239f24c69785fec
sergio <sergiobh@gmail.com>
2013-04-24 17:00:33 -0300

    Banco de dados com registros em todas as tabelas

--- Documentação/sghh.sql
+++ Documentação/sghh.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50610
 File Encoding         : 65001
 
-Date: 2013-04-24 12:05:00
+Date: 2013-04-24 16:55:22
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,12 +24,18 @@ CREATE TABLE `funcionario` (
   `Cpf` char(11) NOT NULL,
   `Senha` varchar(10) NOT NULL,
   PRIMARY KEY (`FuncionarioId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of funcionario
 -- ----------------------------
 INSERT INTO `funcionario` VALUES ('1', 'Sérgio Macedo', '01234567890', '123456');
+INSERT INTO `funcionario` VALUES ('2', 'Lucas Sobrinho', '12345678901', '123456');
+INSERT INTO `funcionario` VALUES ('3', 'Enrique Bonifácio', '23456789012', '123456');
+INSERT INTO `funcionario` VALUES ('4', 'Jorge Pimenta', '34567890123', '123456');
+INSERT INTO `funcionario` VALUES ('5', 'Wesley Nunes', '45678901234', '123456');
+INSERT INTO `funcionario` VALUES ('6', 'Igor Espirito Santo', '56789012345', '123456');
+INSERT INTO `funcionario` VALUES ('7', 'Fábio César', '67890123456', '123456');
 
 -- ----------------------------
 -- Table structure for `leito`
@@ -79,12 +85,13 @@ CREATE TABLE `ocupacao` (
   CONSTRAINT `ocupacao_ibfk_2` FOREIGN KEY (`LeitoId`) REFERENCES `leito` (`LeitoId`),
   CONSTRAINT `ocupacao_ibfk_3` FOREIGN KEY (`FuncCadastro`) REFERENCES `funcionario` (`FuncionarioId`),
   CONSTRAINT `ocupacao_ibfk_4` FOREIGN KEY (`FuncBaixa`) REFERENCES `funcionario` (`FuncionarioId`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of ocupacao
 -- ----------------------------
-INSERT INTO `ocupacao` VALUES ('2', '1', '6', null, '1', '2013-04-23', '11:20:35', null, null, null);
+INSERT INTO `ocupacao` VALUES ('2', '1', '6', 'É importante questionar o quanto a competitividade nas transações comerciais representa uma abertura para a melhoria das posturas dos órgãos dirigentes com relação às suas atribuições.', '3', '2013-04-23', '11:20:35', null, null, null);
+INSERT INTO `ocupacao` VALUES ('3', '2', '1', 'Ainda assim, existem dúvidas a respeito de como o aumento do diálogo entre os diferentes setores produtivos promove a alavancagem dos relacionamentos verticais entre as hierarquias.', '2', '2013-04-24', '15:31:43', null, null, null);
 
 -- ----------------------------
 -- Table structure for `paciente`
@@ -109,7 +116,7 @@ CREATE TABLE `paciente` (
 -- ----------------------------
 -- Records of paciente
 -- ----------------------------
-INSERT INTO `paciente` VALUES ('1', 'Sérgio Macedo da Silva Santos', '01234567890', 'Rua A', '12', 'Frente', 'Eldorado', 'Contagem', 'MG', '1', '1', '2013-03-26 11:06:41');
+INSERT INTO `paciente` VALUES ('1', 'Sérgio Macedo da Silva Santos', '01234567890', 'Rua A', '12', 'Frente', 'Eldorado', 'Contagem', 'MG', '1', '0', '2013-03-26 11:06:41');
 INSERT INTO `paciente` VALUES ('2', 'Márcio Justino', '9876543210', 'Rua D', '51', null, null, null, null, '1', '1', '2013-03-27 21:41:23');
 INSERT INTO `paciente` VALUES ('3', 'Daniela Macedo', '111111111', 'Rua B', '50', null, null, null, null, '1', '1', '2013-03-28 10:57:26');
 
@@ -131,7 +138,7 @@ CREATE TABLE `quarto` (
 INSERT INTO `quarto` VALUES ('1', '1', 'M01', '1');
 INSERT INTO `quarto` VALUES ('2', '1', 'M02', '1');
 INSERT INTO `quarto` VALUES ('3', '1', 'F01', '1');
-INSERT INTO `quarto` VALUES ('4', '1', 'I01', '1');
+INSERT INTO `quarto` VALUES ('4', '1', 'I01', '0');
 
 -- ----------------------------
 -- Table structure for `telefone`
