fb7ab156386e8110eb10431684539eb1402176c5
sergio <sergiobh@gmail.com>
2013-04-24 17:02:32 -0300

    Criação das consultas de Leito

--- application/models/leitomod.php
+++ application/models/leitomod.php
@@ -9,7 +9,8 @@ class LeitoMod extends CI_Model{
     public function statusLeitos(){
         $sql    = "
                     SELECT
-                        Q.Andar                
+                        L.LeitoId
+                        ,Q.Andar                
                         ,Q.Identificacao AS Quarto        
                         ,L.Identificacao AS Leito
                         ,IF(L.Status = '2', 'Arrumação', IF(O.LeitoId IS NULL, 'Liberado', 'Ocupado') ) AS Status
@@ -19,6 +20,7 @@ class LeitoMod extends CI_Model{
                         LEFT JOIN ocupacao O ON O.LeitoId = L.LeitoId AND O.FuncBaixa IS NULL
                     WHERE
                         L.Status != 0
+                        AND Q.Status = 1
                     ORDER BY
                         Q.Andar
                         ,Q.Identificacao
@@ -38,8 +40,35 @@ class LeitoMod extends CI_Model{
     }
 
     public function Listar(){
+        $sql    = "
+                    SELECT
+                        L.LeitoId
+                        ,Q.Andar                
+                        ,Q.Identificacao AS Quarto        
+                        ,L.Identificacao AS Leito
+                        ,IF(L.Status = '0', 'Desativado',  IF(L.Status = '2', 'Arrumação', IF(O.LeitoId IS NULL, 'Liberado', 'Ocupado') ) ) AS Status
+                    FROM
+                        leito L
+                        INNER JOIN quarto Q ON Q.QuartoId = L.QuartoId
+                        LEFT JOIN ocupacao O ON O.LeitoId = L.LeitoId AND O.FuncBaixa IS NULL
+                    WHERE
+                        Q.Status = 1
+                    ORDER BY
+                        Q.Andar
+                        ,Q.Identificacao
+                        ,L.Identificacao
+                    ";
 
-        
+        $query  = $this->db->query($sql);
+
+        $dados = $query->result();
+
+        if(count($dados) > 0){
+            return $dados;
+        }
+        else{
+            return false;
+        }
     }
 }
 ?>
--- application/views/leito/listar.php
+++ application/views/leito/listar.php
@@ -1,7 +1,9 @@
 <div>
 Estamos na view leito/listar
+<br />Obs.: Aqui deverá listar todos os leitos inclusives os desativados, cada registro deverá ter um
+link para ser editado.
 <?php
 	echo '<br /><pre>';
-
+	print_r($Leitos);
 ?>
 </div>
