7c98a68f9125fa77702b8b6364c5e4940175eb2b
sergio <sergiobh@gmail.com>
2013-04-24 17:04:33 -0300

    Criação das consultas de Quarto, ficou pendente de commit

--- application/views/quarto/listar.php
+++ application/views/quarto/listar.php
@@ -1,7 +1,9 @@
 <div>
 Estamos na view quarto/listar
+<br />Obs.: Aqui deverá listar todos os quartos inclusives os desativados, cada registro deverá ter um
+link para ser editado.
 <?php
 	echo '<br /><pre>';
-
+	print_r($Quartos);
 ?>
 </div>
