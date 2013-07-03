f17d04c580ac2ec9a7ba72995d453225ef36dda0
sergio <sergiobh@gmail.com>
2013-07-02 22:57:57 -0300

    Nova versão do banco de dados, vazio! Mas com colunas novas

--- Documentação/sghh.sql
+++ Documentação/sghh.sql
@@ -1,18 +1,3 @@
-/*
-Navicat MySQL Data Transfer
-
-Source Server         : localhost_root
-Source Server Version : 50610
-Source Host           : localhost:3306
-Source Database       : sghh
-
-Target Server Type    : MYSQL
-Target Server Version : 50610
-File Encoding         : 65001
-
-Date: 2013-05-08 21:26:13
-*/
-
 SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 -- Table structure for `funcionario`
@@ -27,17 +12,6 @@ CREATE TABLE `funcionario` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
--- Records of funcionario
--- ----------------------------
-INSERT INTO `funcionario` VALUES ('1', 'Sérgio Macedo', '01234567890', '123456');
-INSERT INTO `funcionario` VALUES ('2', 'Lucas Sobrinho', '12345678901', '123456');
-INSERT INTO `funcionario` VALUES ('3', 'Enrique Bonifácio', '23456789012', '123456');
-INSERT INTO `funcionario` VALUES ('4', 'Jorge Pimenta', '34567890123', '123456');
-INSERT INTO `funcionario` VALUES ('5', 'Wesley Nunes', '45678901234', '123456');
-INSERT INTO `funcionario` VALUES ('6', 'Igor Espirito Santo', '56789012345', '123456');
-INSERT INTO `funcionario` VALUES ('7', 'Fábio César', '67890123456', '123456');
-
--- ----------------------------
 -- Table structure for `leito`
 -- ----------------------------
 DROP TABLE IF EXISTS `leito`;
@@ -49,27 +23,7 @@ CREATE TABLE `leito` (
   PRIMARY KEY (`LeitoId`),
   KEY `QuartoId` (`QuartoId`),
   CONSTRAINT `leito_ibfk_1` FOREIGN KEY (`QuartoId`) REFERENCES `quarto` (`QuartoId`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of leito
--- ----------------------------
-INSERT INTO `leito` VALUES ('1', '1', 'Leito1', '1');
-INSERT INTO `leito` VALUES ('2', '1', 'Leito2', '2');
-INSERT INTO `leito` VALUES ('3', '2', 'Leito1', '1');
-INSERT INTO `leito` VALUES ('4', '2', 'Leito2', '1');
-INSERT INTO `leito` VALUES ('5', '1', 'Leito3', '0');
-INSERT INTO `leito` VALUES ('6', '1', 'Leito4', '1');
-INSERT INTO `leito` VALUES ('7', '1', 'Leito10', '1');
-INSERT INTO `leito` VALUES ('8', '2', 'Sérgio', '1');
-INSERT INTO `leito` VALUES ('9', '5', 'Leito1', '1');
-INSERT INTO `leito` VALUES ('10', '5', 'Leito0', '1');
-INSERT INTO `leito` VALUES ('11', '6', 'Leito1', '1');
-INSERT INTO `leito` VALUES ('12', '5', 'Leito100', '2');
-INSERT INTO `leito` VALUES ('13', '5', 'Leito99', '1');
-INSERT INTO `leito` VALUES ('14', '7', 'Leito1', '1');
-INSERT INTO `leito` VALUES ('15', '8', 'Leito1', '1');
-INSERT INTO `leito` VALUES ('16', '9', 'Leito1', '1');
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for `ocupacao`
@@ -95,13 +49,7 @@ CREATE TABLE `ocupacao` (
   CONSTRAINT `ocupacao_ibfk_2` FOREIGN KEY (`LeitoId`) REFERENCES `leito` (`LeitoId`),
   CONSTRAINT `ocupacao_ibfk_3` FOREIGN KEY (`FuncCadastro`) REFERENCES `funcionario` (`FuncionarioId`),
   CONSTRAINT `ocupacao_ibfk_4` FOREIGN KEY (`FuncBaixa`) REFERENCES `funcionario` (`FuncionarioId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of ocupacao
--- ----------------------------
-INSERT INTO `ocupacao` VALUES ('2', '1', '6', 'É importante questionar o quanto a competitividade nas transações comerciais representa uma abertura para a melhoria das posturas dos órgãos dirigentes com relação às suas atribuições.', '3', '2013-04-23', '11:20:35', null, null, null);
-INSERT INTO `ocupacao` VALUES ('3', '2', '1', 'Ainda assim, existem dúvidas a respeito de como o aumento do diálogo entre os diferentes setores produtivos promove a alavancagem dos relacionamentos verticais entre as hierarquias.', '2', '2013-04-24', '15:31:43', null, null, null);
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for `paciente`
@@ -110,6 +58,7 @@ DROP TABLE IF EXISTS `paciente`;
 CREATE TABLE `paciente` (
   `PacienteId` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `Nome` varchar(100) NOT NULL,
+  `Sexo` tinyint(3) unsigned NOT NULL,
   `Cpf` char(11) NOT NULL,
   `Logradouro` varchar(50) DEFAULT NULL,
   `Numero` varchar(10) DEFAULT NULL,
@@ -117,18 +66,11 @@ CREATE TABLE `paciente` (
   `Bairro` varchar(40) DEFAULT NULL,
   `Cidade` varchar(40) DEFAULT NULL,
   `Estado` char(2) DEFAULT NULL,
-  `Tipo` tinyint(3) unsigned NOT NULL,
+  `Tipo` tinyint(3) unsigned NOT NULL COMMENT '1 - Apartamento, 2 - Enfermária',
   `Status` tinyint(3) unsigned NOT NULL DEFAULT '1',
   `DataHora` datetime NOT NULL,
   PRIMARY KEY (`PacienteId`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of paciente
--- ----------------------------
-INSERT INTO `paciente` VALUES ('1', 'Sérgio Macedo da Silva Santos', '01234567890', 'Rua A', '12', 'Frente', 'Eldorado', 'Contagem', 'MG', '1', '0', '2013-03-26 11:06:41');
-INSERT INTO `paciente` VALUES ('2', 'Márcio Justino', '9876543210', 'Rua D', '51', null, null, null, null, '1', '1', '2013-03-27 21:41:23');
-INSERT INTO `paciente` VALUES ('3', 'Daniela Macedo', '111111111', 'Rua B', '50', null, null, null, null, '1', '1', '2013-03-28 10:57:26');
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for `quarto`
@@ -140,20 +82,7 @@ CREATE TABLE `quarto` (
   `Identificacao` varchar(10) NOT NULL,
   `Status` tinyint(3) unsigned NOT NULL DEFAULT '1',
   PRIMARY KEY (`QuartoId`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of quarto
--- ----------------------------
-INSERT INTO `quarto` VALUES ('1', '1', 'M01', '1');
-INSERT INTO `quarto` VALUES ('2', '1', 'M02', '1');
-INSERT INTO `quarto` VALUES ('3', '1', 'F01', '1');
-INSERT INTO `quarto` VALUES ('4', '1', 'I01', '0');
-INSERT INTO `quarto` VALUES ('5', '2', 'I01', '1');
-INSERT INTO `quarto` VALUES ('6', '3', 'M01', '1');
-INSERT INTO `quarto` VALUES ('7', 'Ala1', 'M01', '1');
-INSERT INTO `quarto` VALUES ('8', 'Ala2', 'M01', '1');
-INSERT INTO `quarto` VALUES ('9', 'Ala3', 'M01', '1');
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for `telefone`
@@ -166,9 +95,4 @@ CREATE TABLE `telefone` (
   PRIMARY KEY (`TelefoneId`),
   KEY `PacienteId` (`PacienteId`),
   CONSTRAINT `telefone_ibfk_1` FOREIGN KEY (`PacienteId`) REFERENCES `paciente` (`PacienteId`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of telefone
--- ----------------------------
-INSERT INTO `telefone` VALUES ('1', '1', '(31) 8811-8927');
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
