052d392da55f74a887277c441762b101e32bf027
Sergio <sergiobh@gmail.com>
2013-11-05 19:50:25 -0200

    Home em restfull

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,13 +4,18 @@ class Home extends CI_Controller {
 
 	public function index()
 	{
+		$Dados['View'] 					= 'home/home';
+		$this->load->view('body/index', $Dados);
+	}
 
+	public function montaIndex(){
 		// Rotina para buscar o status dos leitos
 		$this->load->model('LeitoMod');
 		$Leitos = $this->LeitoMod->statusLeitos();
 		$Dados['Leitos'] 				= $Leitos;
 
-		$Dados['View'] 					= 'home/home';
-		$this->load->view('body/index', $Dados);
+		$Dados['success']				= ($Dados['Leitos']) ? true : false;
+
+		echo json_encode($Dados);
 	}
 }
--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,52 +1,7 @@
 <div class='titulo_page'>Painel de Leitos</div>
 <div class="home">
-	<?php
-		$Leito 		= '';
-		$Quarto 	= '';
-		$Andar		= '';
-		//$i = 1;
-		$FecharDiv  = false;
-		
-		if($Leitos){
-			foreach($Leitos as $Registro) {
 
-			if($Andar != $Registro->Andar){
-				if($Andar != ''){
-					echo '</div></div>';
-				}
-
-				echo '<div class="home_andar">';
-				echo '<div class="home_andar_item">Andar: '.$Registro->Andar.'</div>';
-
-				$Andar 	= $Registro->Andar;
-				$Quarto = '';
-				$FecharDiv = true;
-			}
-
-
-			if($Quarto != $Registro->Quarto){
-				if($Quarto != ''){
-					echo '</div>';
-				}
-
-				echo '<div class="home_quarto">';
-				echo '<div class="home_quarto_item">Quarto: '.$Registro->Quarto.'</div>';
-
-				$Quarto 	= $Registro->Quarto;
-				$FecharDiv = true;
-			}
-
-			echo '<div class="home_leito_item '.$Registro->Status.'">Leito: '.$Registro->Leito.'</div>';
-
-		}
-		}
-			
-		// Fecha div home_quarto
-		echo '</div>';
-
-		// Fecha div home_andar
-		echo '</div>';
-	?>
+	<div id="home_conteudo"></div>
 
 	<div class="clear"></div>
 
@@ -59,3 +14,81 @@
 
 	<div class="clear"></div>
 </div>
+<script type="text/javascript">
+	function RedirecionamentoHome(){
+		$.blockUI({ message: '<h1>Ocorreu um erro no servidor!</h1>' });
+
+		// Efetuar o redirecionamento
+		setTimeout(
+			function(){
+				$.unblockUI();
+			},
+			4000
+		);					
+	}
+
+	$(document).ready(function(){
+		$.blockUI({ message: '<h1>Carregando os dados...</h1>' });
+
+		var Url 		= "<?php echo BASE_URL;?>/home/montaIndex";
+		var Conteudo 	= "";
+
+		$.ajax({
+			type: "get",
+			url: Url,
+			dataType: 'json',
+			success: function(retorno){
+				if(retorno.success){
+					var Leitos	= retorno.Leitos;
+
+					var Leito 			= '';
+					var Quarto 			= '';
+					var Andar			= '';
+					var FecharDiv  		= false;
+
+					for(Reg in Leitos){
+
+						if(Andar != Leitos[Reg].Andar){						
+
+							if(Andar != ""){
+								Conteudo += "</div></div>";
+							}
+
+							Conteudo += '<div class="home_andar">';
+							Conteudo += '<div class="home_andar_item">Andar: '+Leitos[Reg].Andar+'</div>';
+
+							Andar 	  = Leitos[Reg].Andar;
+							Quarto 	  = "";
+							FecharDiv = true;
+						}
+
+						if(Quarto != Leitos[Reg].Quarto){
+							if(Quarto != ""){
+								Conteudo += '</div>';
+							}
+
+							Conteudo += '<div class="home_quarto">';
+							Conteudo += '<div class="home_quarto_item">Quarto: '+Leitos[Reg].Quarto+'</div>';
+
+							Quarto    = Leitos[Reg].Quarto;
+							FecharDiv = true;
+						}
+					
+						Conteudo += '<div class="home_leito_item '+Leitos[Reg].Status+'">Leito: '+Leitos[Reg].Leito+'</div>';
+					}
+
+					Conteudo += '</div></div>';					
+
+					$("#home_conteudo").html(Conteudo);
+					$.unblockUI();
+				}
+				else{
+					RedirecionamentoHome();					
+				}
+			},
+			error: function(){
+				RedirecionamentoHome();
+			}
+		});
+	});
+</script>
