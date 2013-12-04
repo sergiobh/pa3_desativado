c5095d15f8d58d9cfd88fd09e98b71cb629f4b3c
Sergio <sergiobh@gmail.com>
2013-12-03 23:35:16 -0200

    Finalização do projeto SGHH!!!

--- application/views/funcionario/login.php
+++ application/views/funcionario/login.php
@@ -23,10 +23,6 @@
     </tr>
   </table>
 </form>
-<?php /*
-<a id="contrato" href="#inline1" title="Contrato">aasfasfjadklfjasf</a>
-*/?>
-<a data-fancybox-type="iframe" id="contrato" href='<?php echo BASE_URL;?>/contrato'>aasfasfjadklfjasf</a>
 <script type="text/javascript">
 $(document).ready(function(){
 
@@ -37,8 +33,14 @@ $(document).ready(function(){
       return false;
     }
 
-    jConfirm('zfsfhas fsjh slgh wgdgkjh glkjdsgh lskgjh eiug ehrhgiehgeklgh gjlk', 'Contrato de Utilização', function(r) {
-      //jAlert('Confirmed: ' + r, 'Confirmation Results');
+    var MSGContato = '<div class="bloco_msg"><b>I.</b> Não revelar fora do âmbito profissional estado ou informação de qualquer natureza de que tenha conhecimento por força de minhas atribuições, salvo em decorrência de decisão competente na esfera legal ou judicial, bem como de autoridade superior;</div>';
+    MSGContato += '<br /><div class="bloco_msg"><b>II.</b> Utilizar os dados do sistema informatizado de acesso restrito e manter a necessária cautela quando da exibição de dados em tela, impressora ou na gravação em meios eletrônicos, a fim de evitar que deles venham a tomar ciência pessoas não autorizadas;</div>';
+    MSGContato += '<br /><div class="bloco_msg"><b>III.</b> Não me ausentar da estação de trabalho sem encerrar a sessão de uso do sistema, garantindo assim a impossibilidade de acesso indevido por terceiros;</div>';
+    MSGContato += '<br /><div class="bloco_msg"><b>IV.</b> Não revelar minha senha de acesso ao(s) sistema(s) a ninguém e tomar o máximo de cuidado para que ela permaneça somente de meu conhecimento;</div>';
+    MSGContato += '<br /><div class="bloco_msg">Declaro, nesta data, ter ciência e estar de acordo com os procedimentos acima descritos, comprometendo-me a respeitá-los e cumpri-los plena e integralmente, além de manter sempre verdadeiros os dados de instituição e de minha área de competência.</div>';
+
+
+    jConfirm(MSGContato, 'TERMO DE RESPONSABILIDADE', function(r) {
       if(r){
         logar();
       }
@@ -79,14 +81,17 @@ $(document).ready(function(){
         }
         else{
           // Se php retornou erro irá salvar o retorno da div "retorno"
-          alert('Usuário ou senha inválido');
+          //alert('Usuário ou senha inválido');
+
+          jAlert('Usuário ou senha inválido!', 'CONTROLE DE ACESSO');          
+
           $.unblockUI();
         }
       },
       error: function(){
         //$('.retorno_ajax').html('Ocorreu um erro no servidor. Tentar novamente!');
-        alert('Erro no servidor');
-        //$.unblockUI();
+        $.unblockUI();
+        jAlert('Erro no servidor', 'CONTROLE DE ACESSO');
       }
     });
   }  
